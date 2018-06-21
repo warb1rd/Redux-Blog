49c1aa2af28a82fc88bb0b3b3ec80e2207c93097
warb1rd <mahima.m23@gmail.com>
2018-06-21 14:38:26 -0700

    blog start

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -21,6 +22,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3> 
         <ul className="list-group">
           {this.renderPosts()}
---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React,  { Component } from 'react';
+import { Field, ReduxForm } from 'redux-form';
+class PostsNew extends Component {
+  render(){
+    return(
+      <div>
+        New Posts!
+      </div>
+    )
+  }
+}
+
+export default PostsNew;
